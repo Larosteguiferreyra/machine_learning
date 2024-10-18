b9f355b7cc647b955441367d117b470c7e1edbd0
Lucas Jeva Rinaldi <l.jeva@altea-international-school.es>
2024-10-18 14:06:01 +0200

    add the physics for the jump. the player now jumps on spacebar pressed

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ screen = pygame.display.set_mode((window.width, window.height))
 pygame.display.set_caption("Dino dash")
 clock = pygame.time.Clock()
 running = True
+g = 1 #gravity value
 
 # asset setup
-class Ball(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
 
     # Constructor. Pass in the color of the block,
     # and its x and y position
@@ -24,7 +25,6 @@ class Ball(pygame.sprite.Sprite):
        pygame.sprite.Sprite.__init__(self)
 
        # Create an image of the block, and fill it with a color.
-       # This could also be an image loaded from the disk.
        self.image = pygame.Surface([width, height])
        pygame.draw.ellipse(self.image, colour, [0, 0, width, height])
 
@@ -32,12 +32,34 @@ class Ball(pygame.sprite.Sprite):
        # Update the position of this object by setting the values of rect.x and rect.y
        self.rect = self.image.get_rect()
        self.rect.x = window.width * 0.1
-       self.rect.y = floor_y - self.rect.height
+       self.rect.y = window.height // 2
+       self.velocity = 0
 
-ball = Ball("white", window.height * 0.075 , window.height * 0.075)
+    # define gravity
+    def gravity(self):
+        self.velocity += g
+
+    # check if the ball is touching the floor
+    def grounded(self):
+        return self.rect.y >= floor_y - self.rect.height
+
+    # define the update function
+    def update(self):
+        self.rect.y += self.velocity
+
+        if self.grounded():
+            self.rect.y = floor_y - self.rect.height
+            self.velocity = 0
+
+ball = Player("white", window.height * 0.075 , window.height * 0.075)
 
 # game loop
 while running:
+    # process input
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE] and ball.grounded():
+        ball.velocity = -25
+
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -49,6 +71,8 @@ while running:
     # RENDER YOUR GAME HERE.
     screen.blit(ball.image, ball.rect)
     pygame.draw.line(screen, "white", [0, floor_y], [window.width, floor_y], int(window.height * 0.005)) # draw the floor
+    ball.gravity()
+    ball.update()
 
     # flip() the display to print all changes in the screen
     pygame.display.flip()
